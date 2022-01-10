ba4f7c3826e81d1543b89563cf4be29cb07be911
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2022-01-10 16:34:32 +0530

    should have atleast one upper case

---
+++ UserEntryTest.java/src/com/userentrytestJunit/PasswordRule2.java
@@ -0,0 +1,30 @@
+package com.userentrytestJunit;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import org.hamcrest.Matcher;
+
+public class PasswordRule2 {
+	public boolean passwrdRule(String inputStr) {
+		String passwordPattern = "^(?=.*[A-Z])[A-Za-z]{8}$";
+		Pattern regex = Pattern.compile(passwordPattern);
+		java.util.regex.Matcher passwordMatcher = regex.matcher(inputStr);
+		if (passwordMatcher.matches()) {
+			System.out.println(inputStr + " succesfully loggedin and passed second rule.");
+		} else {
+			System.out.println(inputStr + " is invalid password.Kindly input minimum 8 character.");
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		PasswordRule1 psswrd = new PasswordRule1();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please log in using 8 character password having one upper case.");
+		System.out.print("Enter the password : ");
+		String inputStr = sc.nextLine();
+		sc.close();
+		psswrd.passwrdRule(inputStr);
+	}
+}
+
